9bbd39f0a07bb28ae4115a504ffede9d478490bf
sanlangguo <SanLangGuo@outlook.com>
2023-06-20 12:31:21 +0800

    docs: 修改文件位置

--- docs/.vuepress/.temp/internal/pagesComponents.js
+++ docs/.vuepress/.temp/internal/pagesComponents.js
@@ -3,20 +3,18 @@ import { defineAsyncComponent } from 'vue'
 export const pagesComponents = {
   // path: /
   "v-8daa1a0e": defineAsyncComponent(() => import(/* webpackChunkName: "v-8daa1a0e" */"/Users/chenzongqiang/Desktop/learn/learn-notes/docs/.vuepress/.temp/pages/index.html.vue")),
-  // path: /css/css.html
-  "v-a7937e8a": defineAsyncComponent(() => import(/* webpackChunkName: "v-a7937e8a" */"/Users/chenzongqiang/Desktop/learn/learn-notes/docs/.vuepress/.temp/pages/css/css.html.vue")),
-  // path: /js/base-ts.html
-  "v-2f7b5caa": defineAsyncComponent(() => import(/* webpackChunkName: "v-2f7b5caa" */"/Users/chenzongqiang/Desktop/learn/learn-notes/docs/.vuepress/.temp/pages/js/base-ts.html.vue")),
-  // path: /js/js.html
-  "v-51df5a23": defineAsyncComponent(() => import(/* webpackChunkName: "v-51df5a23" */"/Users/chenzongqiang/Desktop/learn/learn-notes/docs/.vuepress/.temp/pages/js/js.html.vue")),
   // path: /mobile/h5-jump-app.html
   "v-434a106a": defineAsyncComponent(() => import(/* webpackChunkName: "v-434a106a" */"/Users/chenzongqiang/Desktop/learn/learn-notes/docs/.vuepress/.temp/pages/mobile/h5-jump-app.html.vue")),
-  // path: /nav/list.html
-  "v-2b876a94": defineAsyncComponent(() => import(/* webpackChunkName: "v-2b876a94" */"/Users/chenzongqiang/Desktop/learn/learn-notes/docs/.vuepress/.temp/pages/nav/list.html.vue")),
   // path: /node/%E5%AD%A6%E4%B9%A0node%E7%9F%A5%E8%AF%86%E6%A1%86%E6%9E%B6.html
   "v-53ca0971": defineAsyncComponent(() => import(/* webpackChunkName: "v-53ca0971" */"/Users/chenzongqiang/Desktop/learn/learn-notes/docs/.vuepress/.temp/pages/node/学习node知识框架.html.vue")),
   // path: /node/%E6%A8%A1%E5%9D%97%E5%8C%96%E4%BB%8B%E7%BB%8D.html
   "v-2a50d1fa": defineAsyncComponent(() => import(/* webpackChunkName: "v-2a50d1fa" */"/Users/chenzongqiang/Desktop/learn/learn-notes/docs/.vuepress/.temp/pages/node/模块化介绍.html.vue")),
+  // path: /js/base-ts.html
+  "v-2f7b5caa": defineAsyncComponent(() => import(/* webpackChunkName: "v-2f7b5caa" */"/Users/chenzongqiang/Desktop/learn/learn-notes/docs/.vuepress/.temp/pages/js/base-ts.html.vue")),
+  // path: /js/js.html
+  "v-51df5a23": defineAsyncComponent(() => import(/* webpackChunkName: "v-51df5a23" */"/Users/chenzongqiang/Desktop/learn/learn-notes/docs/.vuepress/.temp/pages/js/js.html.vue")),
+  // path: /nav/list.html
+  "v-2b876a94": defineAsyncComponent(() => import(/* webpackChunkName: "v-2b876a94" */"/Users/chenzongqiang/Desktop/learn/learn-notes/docs/.vuepress/.temp/pages/nav/list.html.vue")),
   // path: /vue/vue.html
   "v-13c9aef5": defineAsyncComponent(() => import(/* webpackChunkName: "v-13c9aef5" */"/Users/chenzongqiang/Desktop/learn/learn-notes/docs/.vuepress/.temp/pages/vue/vue.html.vue")),
   // path: /%E9%9A%8F%E6%89%8B%E8%AE%B0/%E5%A6%82%E4%BD%95%E7%B3%BB%E7%BB%9F%E5%AD%A6%E4%B9%A0%E4%B8%80%E4%B8%AA%E6%96%B0%E7%9F%A5%E8%AF%86.html
